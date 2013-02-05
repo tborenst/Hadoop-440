ab76a30ed772ec149428bbe81c90feedfeeeff15
Vansi Vallabhaneni <vvallabh@andrew.cmu.edu>
2013-02-04 22:01:34 -0500

    add some debugging stuff

--- projects/01/src/testing/Testing2.java
+++ projects/01/src/testing/Testing2.java
@@ -4,6 +4,6 @@ import processManager.SlaveNode;
 
 public class Testing2 {
 	public static void main(String[] args){
-		SlaveNode slave = new SlaveNode("localhost", 4013, "./serialization/", 5000);
+		SlaveNode slave = new SlaveNode("localhost", 4013, "/afs/andrew.cmu.edu/usr6/vvallabh/public/15440/projects/01/src/serialization/", 5000);
 	}
 }
