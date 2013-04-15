7beecdc7a029b62ad4f412d7db4c3328a3099f0f
Vansi Vallabhaneni <vansivallab@gmail.com>
2013-04-14 22:13:03 -0400

    made the command prompt a little bit nicer

--- projects/03/src/vansitest/ClientTest.java
+++ projects/03/src/vansitest/ClientTest.java
@@ -1,5 +1,6 @@
 package vansitest;
 
+import java.io.Console;
 import java.io.FileNotFoundException;
 
 import org.codehaus.jackson.JsonParseException;
@@ -10,9 +11,18 @@ import system.InValidConfigFileException;
 
 public class ClientTest {
 	public static void main(String[] args) throws JsonParseException, JsonMappingException, FileNotFoundException, InValidConfigFileException {
-		Request r = Request.constructFromFile("C:/Users/vansi/Documents/School/15440/projects/03/src/vansitest/config.json");
-		System.out.println(r.getMapperDirectory());
-		r.exportTo("C:/Users/vansi/Documents/School/15440/projects/03/src/vansitest/configExp.json");
+		 Console c = System.console();
+	    if (c != null) {
+	        // printf-like arguments
+	        c.format("asdf >");
+	        c.format("\nPress ENTER to proceed.\n");
+	        c.readLine();
+	    }
+		
+		
+		//Request r = Request.constructFromFile("C:/Users/vansi/Documents/School/15440/projects/03/src/vansitest/config.json");
+		//System.out.println(r.getMapperDirectory());
+		//r.exportTo("C:/Users/vansi/Documents/School/15440/projects/03/src/vansitest/configExp.json");
 		
 		
 		/*final CommandPrompt c = new CommandPrompt();
