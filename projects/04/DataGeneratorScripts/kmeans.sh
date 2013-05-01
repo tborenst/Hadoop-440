f1364b00a9e0b0562a1db6b920cad8e58fbf7c76
Vansi Vallabhaneni <vansivallab@gmail.com>
2013-05-01 17:12:14 -0400

    python script should work

--- projects/04/DataGeneratorScripts/kmeans.sh
+++ projects/04/DataGeneratorScripts/kmeans.sh
@@ -8,5 +8,5 @@ k=2
 
 
 		echo ********GENERATING $b INPUT POINTS EACH IN $k CLUSTERS 
-		python ./randomclustergen/generaterawdata.py -c $k  -p $b -o input/cluster.csv
+		./randomclustergen/generaterawdata.py -c $k  -p $b -o input/cluster.csv
 
